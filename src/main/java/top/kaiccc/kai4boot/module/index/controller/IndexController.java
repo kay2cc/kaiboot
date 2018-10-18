4e753d12c611d7f5516da0386d06ce841cbbc8f8
xt <xiaokaic@qq.com>
2018-10-18 11:48:13 +0800

    [dev] 增加 欢迎页面

--- src/main/java/top/kaiccc/kai4boot/module/index/controller/IndexController.java
+++ src/main/java/top/kaiccc/kai4boot/module/index/controller/IndexController.java
@@ -1,27 +1,28 @@
 package top.kaiccc.kai4boot.module.index.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import top.kaiccc.kai4boot.common.utils.RedisUtils;
-import top.kaiccc.kai4boot.common.utils.RestResponse;
 
 /**
  * Index Controller
  * @author kaiccc
  * @date 2018-10-09 16:46
  */
-@RestController
+@Controller
 @RequestMapping("/")
 public class IndexController {
 
     @Autowired
     private RedisUtils redisUtils;
 
-    @GetMapping("")
-    public RestResponse index (){
-        return RestResponse.success();
+    @RequestMapping("")
+    public String index (Model model){
+        model.addAttribute("projectName", "kai4boot");
+
+        return "index";
     }
 
 }
